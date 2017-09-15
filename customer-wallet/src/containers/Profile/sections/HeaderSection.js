a70a18481076358d094f76df67776194614a72bb
Venumteam <maxtor2808@gmail.com>
2017-09-15 12:58:13 +0300

    updated profile header

--- customer-wallet/src/containers/Profile/sections/HeaderSection.js
+++ customer-wallet/src/containers/Profile/sections/HeaderSection.js
@@ -1,36 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 // components
 import { Link } from 'react-router'
 import StarRatingComponent from 'react-star-rating-component'
 // constants
 const starCount = 5
+const starValue = 4
 
-export default class ProfileHeader extends Component {
+const ProfileHeader = () => (
+  <div className='profile-header'>
+    <div>
+      <Link to='#' className='profile-header__name'>
+        Awesome seller
+      </Link>
+    </div>
+    <StarRatingComponent
+      name="rate2"
+      starCount={starCount}
+      value={starValue}
+      editing={false}
+    />
+    <br />
+    <span className='profile-header__info'>(250 Reviews, Over $250,000 sales)</span>
+  </div>
+)
 
-  state = { rating: 4 }
-
-  handleStarChange = (nextValue) => {
-    this.setState({ rating: nextValue })
-  }
-
-  render () {
-    const { rating } = this.state
-    return (
-      <div className='profile-header'>
-        <div>
-          <Link to='#' className='profile-header__name'>
-            Awesome seller
-          </Link>
-        </div>
-        <StarRatingComponent
-          name="rate2"
-          starCount={starCount}
-          value={rating}
-          onStarClick={this.handleStarChange}
-        />
-        <br />
-        <span className='profile-header__info'>(250 Reviews, Over $250,000 sales)</span>
-      </div>
-    )
-  }
-}
+export default ProfileHeader
