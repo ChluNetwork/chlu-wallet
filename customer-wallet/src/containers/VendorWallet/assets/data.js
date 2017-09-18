3826f5061536636bb22bfbb475bb853dd2ec8bda
Venumteam <maxtor2808@gmail.com>
2017-09-18 13:30:16 +0300

    added fx converter to vendor wallet page

--- customer-wallet/src/containers/VendorWallet/assets/data.js
+++ customer-wallet/src/containers/VendorWallet/assets/data.js
@@ -1,62 +1,65 @@
 import noProduct from './no-product.png'
 
-const data = [
-  {
-    date: 'September 2017',
-    reviews: [
-      {
-        productTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-        ' Cras bibendum non turpis et sollicitudin. ' +
-        'Aenean ac turpis ac mi molestie faucibus vitae nec diam. ' +
-        'In bibendum sem lorem, et faucibus.',
-        date: 'Sept 05, 2017',
-        price: '1.18732 BTC',
-        rating: 3,
-        review: 'Shinny New Item 1',
-        platform: 'Amazon',
-        productPhoto: noProduct
-      },
-      {
-        productTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-        ' Cras bibendum non turpis et sollicitudin. ' +
-        'Aenean ac turpis ac mi molestie faucibus vitae nec diam.',
-        date: 'Sept 01, 2017',
-        price: '2.12353 BTC',
-        rating: 2,
-        review: 'Shinny New Item 2',
-        platform: 'Exmo',
-        productPhoto: noProduct
-      }
-    ]
-  },
-  {
-    date: 'August 2017',
-    reviews: [
-      {
-        productTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-        ' Cras bibendum non turpis et sollicitudin. ' +
-        'Aenean ac turpis ac mi molestie faucibus vitae nec diam. ' +
-        'In bibendum sem lorem, et faucibus. Cras bibendum non turpis et sollicitudin.',
-        date: 'Aug 15, 2017',
-        price: '1.231241 BTC',
-        rating: 3,
-        review: 'Shinny New Item 3',
-        platform: 'Amazon',
-        productPhoto: noProduct
-      },
-      {
-        productTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-        ' Cras bibendum non turpis et sollicitudin. ' +
-        'Aenean ac turpis ac mi molestie faucibus vitae nec diam.',
-        date: 'Aug 10, 2017',
-        price: '3.45341 BTC',
-        rating: 1,
-        review: 'Shinny New Item 4',
-        platform: 'Exmo',
-        productPhoto: noProduct
-      }
-    ]
-  }
-]
+const vendorData = {
+  total: 14.2332,
+  reviews: [
+    {
+      date: 'September 2017',
+      reviews: [
+        {
+          productTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+          ' Cras bibendum non turpis et sollicitudin. ' +
+          'Aenean ac turpis ac mi molestie faucibus vitae nec diam. ' +
+          'In bibendum sem lorem, et faucibus.',
+          date: 'Sept 05, 2017',
+          price: '1.18732 BTC',
+          rating: 3,
+          review: 'Shinny New Item 1',
+          platform: 'Amazon',
+          productPhoto: noProduct
+        },
+        {
+          productTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+          ' Cras bibendum non turpis et sollicitudin. ' +
+          'Aenean ac turpis ac mi molestie faucibus vitae nec diam.',
+          date: 'Sept 01, 2017',
+          price: '2.12353 BTC',
+          rating: 2,
+          review: 'Shinny New Item 2',
+          platform: 'Exmo',
+          productPhoto: noProduct
+        }
+      ]
+    },
+    {
+      date: 'August 2017',
+      reviews: [
+        {
+          productTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+          ' Cras bibendum non turpis et sollicitudin. ' +
+          'Aenean ac turpis ac mi molestie faucibus vitae nec diam. ' +
+          'In bibendum sem lorem, et faucibus. Cras bibendum non turpis et sollicitudin.',
+          date: 'Aug 15, 2017',
+          price: '1.231241 BTC',
+          rating: 3,
+          review: 'Shinny New Item 3',
+          platform: 'Amazon',
+          productPhoto: noProduct
+        },
+        {
+          productTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+          ' Cras bibendum non turpis et sollicitudin. ' +
+          'Aenean ac turpis ac mi molestie faucibus vitae nec diam.',
+          date: 'Aug 10, 2017',
+          price: '3.45341 BTC',
+          rating: 1,
+          review: 'Shinny New Item 4',
+          platform: 'Exmo',
+          productPhoto: noProduct
+        }
+      ]
+    }
+  ]
+}
 
-export default data
+export default vendorData
--- customer-wallet/src/containers/VendorWallet/index.js
+++ customer-wallet/src/containers/VendorWallet/index.js
@@ -1,22 +1,43 @@
-import React from 'react'
-//components
+import React, { Component } from 'react'
+// utils
+import { getExchangeRates } from 'utils/exchangeReq'
+// libs
+import { fx } from 'money'
+// components
 import PaymentsList from './PaymentsList'
-//styles
+// styles
 import './VendorWallet.css'
-//data
+// data
 import data from './assets/data'
 
-const VendorWallet = () => (
-  <div className="vendor-wallet">
-    <h3>Vendor Wallet</h3>
-    <div className="crypto">
-      <h3>14.2332 BTC</h3>
-      <span>12345.67 USD</span>
-    </div>
-    {
-      data.map(({date, reviews}, index) => <PaymentsList date={date} reviews={reviews} key={index}/>)
-    }
-  </div>
-)
+class VendorWallet extends Component {
+
+  componentWillMount () {
+    getExchangeRates().then((data) => {
+      if (data) {
+        fx.rates = data.rates
+        fx.base = data.base
+      }
+    })
+  }
+
+  getTotalUsd = (value) => fx.convert(value, { from: 'BTC', to: 'USD' }).toFixed(4)
+
+  render () {
+    const totalUsd = this.getTotalUsd(data.total)
+    return (
+      <div className="vendor-wallet">
+        <h3>Vendor Wallet</h3>
+        <div className="crypto">
+          <h3>{data.total} BTC</h3>
+          <span>{totalUsd} USD</span>
+        </div>
+        {
+          data.reviews.map(({date, reviews}, index) => <PaymentsList date={date} reviews={reviews} key={index}/>)
+        }
+      </div>
+    )
+  }
+}
 
 export default VendorWallet
