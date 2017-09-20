a5b3b11169aabfa28a0c616699c7bd1077c86dc5
Venumteam <maxtor2808@gmail.com>
2017-09-20 22:14:20 +0300

    added profile reducer and fetching data for profile

--- customer-wallet/src/containers/Profile/sections/HeaderSection.js
+++ customer-wallet/src/containers/Profile/sections/HeaderSection.js
@@ -1,27 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 // components
 import { Link } from 'react-router'
 import StarRatingComponent from 'react-star-rating-component'
 // constants
 const starCount = 5
-const starValue = 4
 
-const ProfileHeader = () => (
+const ProfileHeader = ({ name, rating, title }) => (
   <div className='profile-header'>
     <div>
-      <Link to='#' className='profile-header__name'>
-        Awesome seller
-      </Link>
+      <Link to='#' className='profile-header__name'>{name}</Link>
     </div>
     <StarRatingComponent
       name='rate2'
       starCount={starCount}
-      value={starValue}
+      value={rating}
       editing={false}
     />
     <br />
-    <span className='profile-header__info'>(250 Reviews, Over $250,000 sales)</span>
+    <span className='profile-header__info'>({title})</span>
   </div>
 )
+
+ProfileHeader.propTypes = {
+  name: PropTypes.string,
+  rating: PropTypes.number,
+  title: PropTypes.string
+}
 
 export default ProfileHeader
