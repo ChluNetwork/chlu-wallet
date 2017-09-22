3d3c150b98bbb9a6032001f28425c83aa41a76f1
Kulpreet Singh <pundit.singh@gmail.com>
2017-09-22 10:46:10 +0200

    Fix token in nock requests, add token to shippable

--- customer-wallet/src/lib/get_utxos.test.js
+++ customer-wallet/src/lib/get_utxos.test.js
@@ -5,7 +5,7 @@ test('load utxos from blocktrail for a given address', () => {
 
   nock('https://api.blockcypher.com:443', {"encodedQueryParams":true})
     .get('/v1/btc/test3/addrs/mjw2BcBvNKkgLvQyYhzRERRgWSUVG7HHTb')
-    .query({"unspentOnly":"true"})
+    .query({"unspentOnly":"true", "token":process.env.BLOCKCYPHER_TOKEN})
     .reply(200, {"address":"mjw2BcBvNKkgLvQyYhzRERRgWSUVG7HHTb","total_received":182464441,"total_sent":600000,"balance":181864441,"unconfirmed_balance":0,"final_balance":181864441,"n_tx":4,"unconfirmed_n_tx":0,"final_n_tx":4,"txrefs":[{"tx_hash":"b71b74674e5ca38c45bfbd0a84172a991a8e67513902f5ef31c9ca6b00d0527c","block_height":1180438,"tx_input_n":-1,"tx_output_n":1,"value":181864441,"ref_balance":363928882,"spent":false,"confirmations":19583,"confirmed":"2017-08-29T10:42:28Z","double_spend":false}],"tx_url":"https://api.blockcypher.com/v1/btc/test3/txs/"},
            [ 'Server',
              'nginx/1.13.4',
--- customer-wallet/src/lib/push_tx.js
+++ customer-wallet/src/lib/push_tx.js
@@ -10,8 +10,8 @@ class PushTx {
   push (txHash) {
     return new Promise((resolve, reject) => {
       this.blockchainClient.pushTX(txHash, (err, tx) => {
-        if ( err || !isEmpty(tx.error) ) {
-          reject(err || tx.error)
+        if ( err  ) {
+          reject(err )
         } else {
           resolve(tx)
         }
