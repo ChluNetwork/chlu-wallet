ab3f09e0a08d299d017b8a86f1bbcdee9a74a899
Venumteam <maxtor2808@gmail.com>
2017-09-26 16:04:58 +0300

    added actions import

--- shared-libraries/src/index.js
+++ shared-libraries/src/index.js
@@ -1,5 +1,7 @@
 import reducers from './reducers'
+import actions from './reducers/actions'
 
 export {
-  reducers
+  reducers,
+  actions
 }
---
+++ shared-libraries/src/reducers/actions.js
@@ -0,0 +1,17 @@
+import * as checkout from './data/checkout'
+import * as fxRates from './data/fxRates'
+import * as payment from './data/payment'
+import * as profile from './data/profile'
+import * as vendorWallet from './data/vendorWallet'
+
+const dataActions = {
+  checkout,
+  fxRates,
+  payment,
+  profile,
+  vendorWallet
+}
+
+export default {
+  dataActions
+}
