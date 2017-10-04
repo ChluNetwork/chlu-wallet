f55982690995c7f6cf778c24cfbfca8425dd1ab0
Venumteam <maxtor2808@gmail.com>
2017-10-04 10:05:19 +0300

    merge changes with master

--- customer-wallet/src/containers/Customer/CustomerWallet/CustomerWalletForm/index.js
+++ customer-wallet/src/containers/Customer/CustomerWallet/CustomerWalletForm/index.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import { setRatingForCustomerWallet } from 'store/modules/components/CustomerWallet'
 import { actions } from 'shared-libraries/lib'
 import { formValueSelector } from 'redux-form'
+import { setFxRates } from 'lib/fxRates'
 // libs
 import { toastr } from 'react-redux-toastr'
 // components
@@ -23,7 +24,9 @@ class CustomerWalletFormWrapper extends Component {
     isReviewOpen: PropTypes.bool,
     loading: PropTypes.bool,
     rating: PropTypes.number.isRequired,
-    setRating: PropTypes.func.isRequired
+    setRating: PropTypes.func.isRequired,
+    rates: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+    getRates: PropTypes.func.isRequired
   }
 
   handleSubmit = (data) => {
@@ -65,12 +68,14 @@ const selector = formValueSelector('customer-wallet-form')
 const mapStateToProps = state => ({
   isReviewOpen: selector(state, 'reviewOpen'),
   loading: state.data.payment.loading,
-  rating: state.components.customerWallet.rating
+  rating: state.components.customerWallet.rating,
+  rates: state.data.fxRates.rates
 })
 
 const mapDispatchToProps = dispatch => ({
   submitPayment: data => dispatch(submitPayment(data)),
-  setRating: data => dispatch(setRatingForCustomerWallet(data))
+  setRating: data => dispatch(setRatingForCustomerWallet(data)),
+  getRates: () => dispatch(getRates())
 })
 
 export default connect(
