77f6109bf89f06a1ca5ebef428c9551239895176
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-01-15 14:01:51 +0100

    re-enable uglifyjs

--- customer-wallet/config/webpack.config.prod.js
+++ customer-wallet/config/webpack.config.prod.js
@@ -260,11 +260,13 @@ module.exports = {
     // Otherwise React will be compiled in the very slow development mode.
     new webpack.DefinePlugin(env.stringified),
     // Minify the code.
-    /*
     new UglifyJsPlugin({
       sourceMap: shouldUseSourceMap,
+      uglifyOptions: {
+        mangle: false,
+        compress: false
+      }
     }),
-    */
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
       filename: cssFilename,
