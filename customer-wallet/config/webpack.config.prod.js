99860abc232c11deb6655f96f87533f9e9aa5251
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-01-15 13:26:26 +0100

    disabled uglifyjs due to issues

--- customer-wallet/config/webpack.config.prod.js
+++ customer-wallet/config/webpack.config.prod.js
@@ -260,9 +260,11 @@ module.exports = {
     // Otherwise React will be compiled in the very slow development mode.
     new webpack.DefinePlugin(env.stringified),
     // Minify the code.
+    /*
     new UglifyJsPlugin({
       sourceMap: shouldUseSourceMap,
     }),
+    */
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
       filename: cssFilename,
