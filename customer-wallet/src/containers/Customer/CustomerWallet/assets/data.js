a14ba6f9a3cba6b672b894c6ff68dd2f4fb8be7c
Brian McSweeney <brian.mcsweeney@gmail.com>
2018-05-19 14:22:39 -0400

    added LN button

--- customer-wallet/src/containers/Customer/CustomerWallet/assets/data.js
+++ customer-wallet/src/containers/Customer/CustomerWallet/assets/data.js
@@ -1,10 +1,12 @@
 import BTC from 'images/svg/cryproIcons/BITCOIN.svg'
+import LN from 'images/svg/cryproIcons/LN.png'
 import LTC from 'images/svg/cryproIcons/LTC.svg'
 import ZCASH from 'images/svg/cryproIcons/ZCASH.svg'
 import ETH from 'images/svg/cryproIcons/ETH.svg'
 import BCC from 'images/svg/cryproIcons/BCC.svg'
 // blue
 import BTCBlue from 'images/svg/cryproIcons/BITCOIN-blue.svg'
+import LNBlue from 'images/svg/cryproIcons/LN-blue.png'
 import LTCBlue from 'images/svg/cryproIcons/LTC-blue.svg'
 import ZCASHBlue from 'images/svg/cryproIcons/ZCASH-blue.svg'
 import ETHBlue from 'images/svg/cryproIcons/ETH-blue.svg'
@@ -12,6 +14,7 @@ import BCCBlue from 'images/svg/cryproIcons/BCC-blue.svg'
 
 const buttonsData = [
   { label: 'BTC', active: true, icon: BTC, iconBlue: BTCBlue },
+  { label: 'LN', active: false, icon: LN, iconBlue: LNBlue },
   { label: 'LTC', active: false, icon: LTC, iconBlue: LTCBlue },
   { label: 'ZCash', active: false, icon: ZCASH, iconBlue: ZCASHBlue },
   { label: 'Ether', active: false, icon: ETH, iconBlue: ETHBlue },
