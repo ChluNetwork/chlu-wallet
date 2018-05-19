b794c556f09665c2b5cd0c70716563a9fc05c260
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-05-19 22:15:12 +0200

    added lightning network first implementation

--- customer-wallet/src/helpers/ln.js
+++ customer-wallet/src/helpers/ln.js
@@ -1,26 +1,32 @@
-import lnService from 'ln-service'
+import path from 'path'
+import axios from 'axios'
 
 export default class Lightning {
 
-    constructor(host = 'localhost:10009') {
-        this.lnd = lnService.lightningDaemon({
-            host
-        });
+    constructor(host) {
+        this.lnd = axios.create({
+            baseURL: path.join(host, 'v0')
+        })
     }
 
-    async getInfo() {
-        return new Promise((resolve, reject) => {
-            lnService.getWalletInfo({ lnd: this.lnd }, (error, result) => {
-                if (error) reject(error); else resolve(result)
-            });
-        })
+    async getInvoices() {
+        return this.lnd.get('/invoices')
+    }
+
+    async getInvoice(id) {
+        return this.lnd.get(`/invoices/${id}`)
     }
 
     async generateInvoice(amount) {
         // should return invoice in string form or an ID to pass to customer
+        return this.lnd.post('/invoices', {
+            description: 'Chlu Payment',
+            tokens: amount // amount in SATOSHI
+        })
     }
 
     async payInvoice(invoice) {
         // should pay the invoice and return proof of payment
+        return this.lnd.post('/payments', invoice)
     }
 }
