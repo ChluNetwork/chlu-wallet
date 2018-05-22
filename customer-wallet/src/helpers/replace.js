210a7915e74404a09c524d5412919f995979d6a4
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-05-22 15:28:38 +0200

    add note about replace.js

--- customer-wallet/src/helpers/replace.js
+++ customer-wallet/src/helpers/replace.js
@@ -1,2 +1,11 @@
+/*
+
+Eventually, this thing should be replaced with dispatch(push(path))
+or with <Link> componenets from react-router-dom.
+
+However this hack works as well so for now I'm keeping it until we
+refactor all the points in which it's currently used
+
+*/
 
 export default path => window.appHistory.push(path)
