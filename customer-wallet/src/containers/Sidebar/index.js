3737acdab544dbb3cf179893203a83fcfebb4471
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-05-30 10:51:05 +0200

    refactor Drawer to Sidebar

--- customer-wallet/src/containers/Sidebar/index.js
+++ customer-wallet/src/containers/Sidebar/index.js
@@ -2,16 +2,23 @@ import React from 'react'
 import { func, bool } from 'prop-types'
 import { connect } from 'react-redux'
 // components
-import { push } from 'react-router-redux'
 import Drawer from '@material-ui/core/Drawer'
 import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+// redux
+import { push } from 'react-router-redux'
+import { toggleDrawer } from 'store/modules/ui/drawer'
 // icons
 import StarIcon from '@material-ui/icons/Star';
 import BitcoinIcon from '@material-ui/icons/AccountBalanceWallet';
 import PaymentIcon from '@material-ui/icons/Send';
 import SettingsIcon from '@material-ui/icons/Settings'
 
-const DrawerComponent = ({ toggleDrawer, drawerOpen, push }) => {
+const Sidebar = ({ toggleDrawer, drawerOpen, push }) => {
+
+  function nav(x) {
+    toggleDrawer()
+    push(x)
+  }
 
   return (
     <Drawer
@@ -19,19 +26,19 @@ const DrawerComponent = ({ toggleDrawer, drawerOpen, push }) => {
       open={drawerOpen}
     >
       <List component='nav'>
-        <ListItem button onClick={() => push('/claim')}>
+        <ListItem button onClick={() => nav('/claim')}>
           <ListItemIcon><StarIcon/></ListItemIcon>
           <ListItemText primary='My Reputation' />
         </ListItem>
-        <ListItem button onClick={() => push('/transactions')}>
+        <ListItem button onClick={() => nav('/transactions')}>
           <ListItemIcon><BitcoinIcon/></ListItemIcon>
           <ListItemText primary='Transactions' />
         </ListItem>
-        <ListItem button onClick={() => push('/pay')}>
+        <ListItem button onClick={() => nav('/pay')}>
           <ListItemIcon><PaymentIcon/></ListItemIcon>
           <ListItemText primary='Pay' />
         </ListItem>
-        <ListItem button onClick={() => push('/settings')}>
+        <ListItem button onClick={() => nav('/settings')}>
           <ListItemIcon><SettingsIcon/></ListItemIcon>
           <ListItemText primary='Settings' />
         </ListItem>
@@ -40,18 +47,23 @@ const DrawerComponent = ({ toggleDrawer, drawerOpen, push }) => {
   )
 }
 
-DrawerComponent.propTypes = {
+Sidebar.propTypes = {
   toggleDrawer: func,
   drawerOpen: bool
 }
 
+const mapStateToProps = state => ({
+  drawerOpen: state.ui.drawer.open
+})
+
 function mapDispatchToProps(dispatch) {
   return {
-    push: x => dispatch(push(x))
+    push: x => dispatch(push(x)),
+    toggleDrawer: () => dispatch(toggleDrawer())
   }
 }
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
-)(DrawerComponent)
+)(Sidebar)
