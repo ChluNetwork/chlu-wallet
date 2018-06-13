e90b1891d3fec52ceac9b9a60559a6a94005d0c4
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-06-13 13:19:17 +0200

    hook up crawler to UI

--- src/containers/SignupWizard/WizardSteps/businessCrawlerForm/index.js
+++ src/containers/SignupWizard/WizardSteps/businessCrawlerForm/index.js
@@ -8,6 +8,7 @@ import { withStyles } from '@material-ui/core'
 
 // redux
 import { connect } from 'react-redux'
+import { startYelpCrawler } from 'store/modules/data/crawler';
 
 // redux form
 import { reduxForm } from 'redux-form'
@@ -37,8 +38,8 @@ const style = {
 };
 
 const submit = (values, dispatch, props) => {
-  // print the form values to the console
   console.log(values)
+  dispatch(startYelpCrawler(values['yelp-email']))
 }
 
 
