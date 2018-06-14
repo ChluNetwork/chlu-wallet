777d4a0e9444c6829ba887f5749fcc746b34b424
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-06-14 14:16:08 +0200

    fix indentation

--- src/containers/SignupWizard/index.js
+++ src/containers/SignupWizard/index.js
@@ -16,80 +16,80 @@ import { pick } from 'lodash'
 
 function SignupWizard(props) {
 
-    function downloadWallet() {
-        const { wallet, walletCreated } = props
-        if (wallet && wallet.did) {
-            downloadWalletFile(pick(wallet, ['did', 'bitcoinMnemonic', 'testnet']))
-        } else {
-            downloadWalletFile(walletCreated)
-        }
-        props.setWalletSaved(true)
+  function downloadWallet() {
+    const { wallet, walletCreated } = props
+    if (wallet && wallet.did) {
+      downloadWalletFile(pick(wallet, ['did', 'bitcoinMnemonic', 'testnet']))
+    } else {
+      downloadWalletFile(walletCreated)
     }
+      props.setWalletSaved(true)
+  }
 
-    function areWalletKeysSaved() {
-        const { walletSaved } = props
-        return walletSaved
+  function areWalletKeysSaved() {
+    const { walletSaved } = props
+    return walletSaved
+  }
+
+  function validate(step) {
+    const { wallet } = props
+    if (step === 1 && !(areWalletKeysSaved() || (wallet && wallet.did))) {
+      toastr.warning(
+        'Please save your wallet',
+        'Once you have saved it you will be able to access the wallet'
+      )
+      return false
     }
+    return true
+  }
 
-    function validate(step) {
-        const { wallet } = props
-        if (step === 1 && !(areWalletKeysSaved() || (wallet && wallet.did))) {
-            toastr.warning(
-                'Please save your wallet',
-                'Once you have saved it you will be able to access the wallet'
-            )
-            return false
-        }
-        return true
+  function onChangeStep(from, to) {
+    const { walletCreated, createWallet, resetWallet, setWallet, wallet } = props
+    if (to === 1 && !walletCreated) createWallet()
+    if (to === 2 && !(wallet && wallet.did)) {
+      // set and save full wallet
+      setWallet(walletCreated)
+      saveWalletToLocalStorage(walletCreated)
+      resetWallet() // deletes temp wallet
+      toastr.success('Wallet Created', 'Your Wallet is ready to go!')
     }
+  }
 
-    function onChangeStep(from, to) {
-        const { walletCreated, createWallet, resetWallet, setWallet, wallet } = props
-        if (to === 1 && !walletCreated) createWallet()
-        if (to === 2 && !(wallet && wallet.did)) {
-            // set and save full wallet
-            setWallet(walletCreated)
-            saveWalletToLocalStorage(walletCreated)
-            resetWallet() // deletes temp wallet
-            toastr.success('Wallet Created', 'Your Wallet is ready to go!')
-        }
-    }
-
-    const initialStep = props.wallet.did ? 2 : (props.walletCreated ? 1 : 0)
+  const initialStep = props.wallet.did ? 2 : (props.walletCreated ? 1 : 0)
 
   return <Wizard
-             validate={validate}
-             currentStep={initialStep}
-             onChangeStep={onChangeStep}
-             finishButtonClick={props.finishClicked}
-             
-             steps={[
-               {
-                 stepName: '1: Create Your Wallet',
-                 stepComponent: Step1,
-                 stepId: 'get started',
-                 stepProps: {
-                   ...props
-                 }
-               },
-               {
-                 stepName: '2: Save Your D.I.D.',
-                 stepComponent: Step2,
-                 stepId: 'about',
-                 stepProps: {
-                   ...props,
-                   downloadWallet
-                 }
-               },
-               {
-                 stepName: '3: Claim Your Reputation',
-                 stepComponent: Step3,
-                 stepId: 'reviews'
-               },
-             ]}
-             title="Let's Get Started"
-             subtitle='Follow The Three Easy Steps Below To Begin'
-         />
+    validate={validate}
+    currentStep={initialStep}
+    onChangeStep={onChangeStep}
+    finishButtonClick={props.finishClicked}
+      
+    steps={[
+      {
+        stepName: '1: Create Your Wallet',
+        stepComponent: Step1,
+        stepId: 'get started',
+        stepProps: {
+          ...props
+        }
+      },
+      {
+        stepName: '2: Save Your D.I.D.',
+        stepComponent: Step2,
+        stepId: 'about',
+        stepProps: {
+          ...props,
+          downloadWallet
+        }
+      },
+      {
+        stepName: '3: Claim Your Reputation',
+        stepComponent: Step3,
+        stepId: 'reviews'
+      },
+    ]}
+    title="Let's Get Started"
+    subtitle='Follow The Three Easy Steps Below To Begin'
+  />
 }
 
 const mapStateToProps = store => ({
@@ -100,11 +100,11 @@ const mapStateToProps = store => ({
 })
 
 const mapDispatchToProps = {
-    finishClicked,
-    createWallet,
-    setWallet,
-    resetWallet,
-    setWalletSaved
+  finishClicked,
+  createWallet,
+  setWallet,
+  resetWallet,
+  setWalletSaved
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignupWizard)
