94123d5f093dec877b2b3a987d218162f36acbca
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-06-15 11:23:04 +0200

    implemented reviews I Wrote component

---
+++ src/containers/ReviewsIWrote/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { get, flatten } from 'lodash'
+// redux
+import { connect } from 'react-redux'
+import { compose } from 'recompose'
+// components
+import Reviews from 'components/Reviews'
+// hoc
+import withTransactions from 'containers/Hoc/withTransactions'
+// helpers
+import { getAddress } from 'helpers/wallet'
+
+class ReviewsIWrote extends Component {
+
+  render() {
+    const { wallet, reviews, loading, error } = this.props
+    const address = getAddress(wallet)
+
+    let transformedReviews = Object.values(reviews)
+      .filter(r => r && !r.loading && !r.error && r.customer_address === address)
+    console.log(transformedReviews)
+    transformedReviews = transformedReviews.map(r => ({
+        subject: {
+          name: 'Vendor'
+        },
+        author: {
+          name: 'You'
+        },
+        platform: {
+          name: 'chlu',
+          url: 'chlu.io'
+        },
+        review: {
+          text: r.review_text
+        },
+        rating: {
+          min: 1,
+          value: r.rating,
+          max: 5
+        },
+        verifiable: true,
+        proof: true
+      }))
+    console.log(transformedReviews)
+
+    return <Reviews reviews={transformedReviews} loading={loading} />
+  }
+
+}
+
+const mapStateToProps = state => ({
+  reviews: state.data.reviews.reviews,
+  loading: state.data.transactions.loading,
+  error: state.data.transactions.error,
+  transactions: get(state, 'data.transactions.data.txs', []),
+  wallet: state.data.wallet
+})
+
+export default compose(
+  connect(mapStateToProps),
+  withTransactions
+)(ReviewsIWrote)
