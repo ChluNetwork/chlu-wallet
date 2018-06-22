b0216f85c26e5931959af07403d059f8037897ad
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-06-22 17:16:30 +0200

    delete dead code

--- src/containers/Wallet/index.js
+++ src/containers/Wallet/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Switch, Redirect } from 'react-router'
-import CreateWallet from './Create/index'
 import ImportWallet from './Import/index'
 import { connect } from 'react-redux';
 
@@ -8,7 +7,6 @@ function WalletRouter({ wallet }) {
   const emptyWallet = !wallet || !wallet.did
   return <Switch>
     {!emptyWallet && <Redirect to='/reputation'/>}
-    <Route path='/setup/create' component={CreateWallet} />
     <Route path='/setup/import' component={ImportWallet} />
     <Redirect from='/setup' to='/homepage' />
   </Switch>
