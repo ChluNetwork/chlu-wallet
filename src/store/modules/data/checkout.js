c590d9da063bbdbe092c621c679d9d4d11dc2968
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-07-04 17:30:58 +0200

    can now pay custom PoPR by link with multihash

--- src/store/modules/data/checkout.js
+++ src/store/modules/data/checkout.js
@@ -1,7 +1,6 @@
 import { createAction, handleActions } from 'redux-actions'
 import { requestPopr } from 'helpers/marketplace'
-// data
-import checkoutData from 'fixtures/checkout'
+import { getChluIPFS } from 'helpers/ipfs'
 
 // ------------------------------------
 // Constants
@@ -27,20 +26,39 @@ export const fetchCheckoutDataLoading = createAction(FETCH_CHECKOUT_DATA_LOADING
 // Thunks
 // ------------------------------------
 
-export function getCheckout () {
+export function getCheckout (poprMultihash) {
+  console.log(poprMultihash)
   return async dispatch => {
     dispatch(fetchCheckoutDataLoading(true))
+    let popr = null
     try {
-      // TODO: replace fixture data with stuff from PoPR
-      // TODO: handle error in UI
-      const vendorId = process.env.REACT_APP_VENDOR_ID || 'Qmtest'
-      const url = process.env.REACT_APP_MARKETPLACE_URL || 'http://localhost:4000'
-      const popr = await requestPopr(url, vendorId, {
-        // TODO: fix the placeholder price
-        amount: checkoutData.price * 10000,
-        currency_symbol: 'tBTC'
-      })
-      dispatch(fetchCheckoutDataSuccess(Object.assign({}, popr, checkoutData)))
+      if (poprMultihash) {
+        console.log('Reading PoPR at ' + poprMultihash)
+        const chluIpfs = await getChluIPFS()
+        // TODO: create API calls in Chlu to fetch PoPR instead of this manual thing
+        const buffer = await chluIpfs.instance.ipfsUtils.get(poprMultihash)
+        popr = await chluIpfs.instance.protobuf.PoPR.decode(buffer)
+        popr.multihash = poprMultihash
+        // TODO: validation
+        console.log('Read PoPR at ' + poprMultihash)
+      } else {
+        const vendorId = process.env.REACT_APP_VENDOR_ID || 'Qmtest'
+        const url = process.env.REACT_APP_MARKETPLACE_URL || 'http://localhost:4000'
+        console.log(`Requesting PoPR to ${url} for vendor ${vendorId}`)
+        popr = await requestPopr(url, vendorId, {
+          // TODO: fix the placeholder price
+          amount: 25 * 10000,
+          currency_symbol: 'tBTC'
+        })
+        popr.multihash = null
+        console.log(`Requested PoPR to ${url} for vendor ${vendorId}`)
+      }
+      // TODO: get vendor_address properly instead of using the hardcoded address
+      if (!popr.vendor_address) {
+        popr.vendor_address = process.env.REACT_APP_VENDOR_ADDRESS || 'ms4TpM57RWHnEq5PRFtfJ8bcdiXoUE3tfv'
+      }
+      console.log(popr)
+      dispatch(fetchCheckoutDataSuccess(Object.assign({}, popr)))
       return popr
     } catch (error) {
       dispatch(fetchCheckoutDataError(error.message || error))
