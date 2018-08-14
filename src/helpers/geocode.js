72f805d81e2af333da1848365166d79b6130b555
johnweisz <jana_tb@live.com>
2018-08-14 17:52:03 +0200

    show business location on map

---
+++ src/helpers/geocode.js
@@ -0,0 +1,26 @@
+const ENDPOINT = 'https://api.tiles.mapbox.com';
+const SOURCE = 'mapbox.places';
+const ACCESS_TOKEN = 'pk.eyJ1Ijoiam9obndlaXN6IiwiYSI6ImNqa3NheWoyNTQzMHkzcW8zem4waTMyMmkifQ._qzWXiBScWpNONx6BxWvgg';
+
+/**
+ * @param {string} query The location to geocode, as human readable text string (e.g. _"Silent Hill, Pennsylvania"_).
+ * @returns {Promise<{ latitude: number, longitude: number }>}
+ */
+export async function geocode(query) {
+  // let searchTime = new Date();
+  let url = `${ENDPOINT}/geocoding/v5/${SOURCE}/${encodeURIComponent(query)}.json?access_token=${ACCESS_TOKEN}`;
+  let response = await fetch(url);
+  let responseJson = await response.json();
+
+  console.log(responseJson);
+
+  if (responseJson.features && responseJson.features[0]) {
+    return {
+      latitude: responseJson.features[0].center[0],
+      longitude: responseJson.features[0].center[1],
+      bounds: responseJson.features[0].bbox
+    };
+  } else {
+    return undefined;
+  }
+}
