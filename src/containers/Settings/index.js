4315f4ce9321d38f96265d94f04da48b3a886a89
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-09-18 15:59:15 +0200

    fix editing profile from Settings

--- src/containers/Settings/index.js
+++ src/containers/Settings/index.js
@@ -3,32 +3,28 @@ import { object } from 'prop-types'
 import { get } from 'lodash'
 
 // components
-import { Button, Card, CardContent, CardActions, CardHeader, Divider, Grid, InputAdornment } from '@material-ui/core'
-import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core'
+import { Button, Card, CardContent, CardActions, CardHeader, Divider, LinearProgress } from '@material-ui/core'
 import { Avatar, withStyles, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
 
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 
-import CustomInput from 'components/MaterialDashboardPro/CustomInput';
-import ProfileImageUpload from 'containers/ProfileImageUpload'
 import ReactCopyToClipBoard from 'react-copy-to-clipboard'
-import BusinessLocationField from 'containers/Profile/BusinessLocationField';
+import ProfileForm from 'containers/Profile/ProfileForm'
 
 // redux
 import { compose } from 'recompose';
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
-import { fetchProfile, updateProfile, businessTypes } from 'store/modules/ui/profile';
+import { fetchProfile, updateProfile } from 'store/modules/ui/profile';
 import { fetchBalance } from 'store/modules/data/wallet';
+import { submit, isDirty, isSubmitting } from 'redux-form'
 
 // helpers
 import { downloadWallet, getAddress } from 'helpers/wallet';
 
 // icons
-import Email from '@material-ui/icons/Email';
-import Face from '@material-ui/icons/Face';
 import DownloadIcon from '@material-ui/icons/FileDownload'
 import UserIcon from '@material-ui/icons/Person'
 import WalletIcon from '@material-ui/icons/AccountBalanceWallet'
@@ -37,8 +33,6 @@ import KeyIcon from '@material-ui/icons/Lock'
 import AccountBoxIcon from '@material-ui/icons/AccountBox'
 import SaveIcon from '@material-ui/icons/Save';
 import BalanceIcon from '@material-ui/icons/AttachMoney'
-import Phone from '@material-ui/icons/Phone'
-import Web from '@material-ui/icons/Web'
 
 
 const styles = theme => ({
@@ -68,25 +62,11 @@ function TabContainer(props) {
 }
 
 class Settings extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profile: {},
-      dirty: false,
-      loading: false
-    };
-  }
 
   async componentDidMount() {
     const didId = get(this.props.wallet, 'did.publicDidDocument.id', '');
-    this.setState({ loading: true })
     this.props.fetchBalance()
-    const profile = await this.props.fetchProfile(didId)
-    this.setState({
-      profile,
-      dirty: false,
-      loading: false
-    })
+    await this.props.fetchProfile(didId)
   }
 
   static propTypes = {
@@ -103,18 +83,20 @@ class Settings extends Component {
     }
   };
 
-  handleDownload () {
+  handleDownload = () => {
     downloadWallet(this.props.wallet)
   }
 
-  async saveProfile () {
-    await this.props.updateProfile(this.state.profile)
+  save = async () => {
+    return await this.props.submit('profile')
   }
 
-  change(event, stateName) {
-    const value = get(event, 'target.value', event)
-    console.log(stateName, value)
-    this.setState({ dirty: true, profile: { ...this.state.profile, [stateName]: value } });
+  updateProfile = async profile => {
+    return await this.props.updateProfile(profile)
+  }
+
+  viewPublicProfile = () => {
+    return this.props.push(`/profile/${this.props.didId}`)
   }
 
   render () {
@@ -151,9 +133,7 @@ class Settings extends Component {
   }
 
   renderProfile() {
-    const { classes } = this.props
-    const { dirty, profile } = this.state
-    const isBusiness = get(profile, 'type') === 'business'
+    const { classes, profile, loading, dirty, submitting } = this.props
     return (
       <Card className={classes.card}>
         <CardHeader
@@ -162,11 +142,19 @@ class Settings extends Component {
           subheader='Your Profile Page'
         />
         <CardContent>
-          {isBusiness ? this.renderBusiness() : this.renderUser()}
+          {loading && <LinearProgress />}
+          {!loading && <ProfileForm
+            userType={get(profile, 'type')}
+            initialValues={profile}
+            onSubmit={this.updateProfile}
+          />}
         </CardContent>
         <CardActions>
-          <Button variant='raised' onClick={this.saveProfile.bind(this)} disabled={!dirty}>
-            <SaveIcon/> Save
+          <Button variant='raised' onClick={this.save} disabled={!dirty || loading || submitting}>
+            <SaveIcon/> {loading ? 'Loading...' : (submitting ? 'Saving...' : (dirty ? 'Save Changes' : 'Saved'))}
+          </Button>
+          <Button onClick={this.viewPublicProfile} disabled={loading}>
+            <UserIcon/> View Public Profile
           </Button>
         </CardActions>
       </Card>
@@ -234,249 +222,16 @@ class Settings extends Component {
       </Card>
     )
   }
-
-  renderUser() {
-    const { classes } = this.props;
-    const { profile } = this.state
-
-    return (
-      <Grid container justify='center' spacing={16}>
-        <Grid item xs={12} sm={12} md={12}>
-          <ProfileImageUpload />
-        </Grid>
-        <Grid item xs={12} sm={12} md={5}>
-          <CustomInput
-            success={this.state.emailState === 'success'}
-            error={this.state.emailState === 'error'}
-            labelText={
-              <span>
-                Email
-              </span>
-            }
-            id='email'
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              onChange: event => this.change(event, 'email', 'length', 3),
-              value: profile.email || "",
-              endAdornment: (
-                <InputAdornment position='end' className={classes.inputAdornment}>
-                  <Email className={classes.inputAdornmentIcon} />
-                </InputAdornment>
-              )
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={5}>
-          <CustomInput
-            success={this.state.usernameState === 'success'}
-            error={this.state.usernameState === 'error'}
-            labelText={
-              <span>
-                User Name
-              </span>
-            }
-            id='username'
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              onChange: event => this.change(event, 'username', 'length', 3),
-              value: profile.username || "",
-              endAdornment: (
-                <InputAdornment position='end' className={classes.inputAdornment}>
-                  <Face className={classes.inputAdornmentIcon} />
-                </InputAdornment>
-              )
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={5}>
-          <CustomInput
-            success={this.state.firstnameState === 'success'}
-            error={this.state.firstnameState === 'error'}
-            labelText={
-              <span>
-                First Name
-              </span>
-            }
-            id='firstname'
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              onChange: event => this.change(event, 'firstname', 'length', 3),
-              value: profile.firstname || "",
-              endAdornment: (
-                <InputAdornment position='end' className={classes.inputAdornment}>
-                  <Face className={classes.inputAdornmentIcon} />
-                </InputAdornment>
-              )
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={5}>
-          <CustomInput
-            success={this.state.lastnameState === 'success'}
-            error={this.state.lastnameState === 'error'}
-            labelText={
-              <span>
-                Last Name
-              </span>
-            }
-            id='lastname'
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              onChange: event => this.change(event, 'lastname', 'length', 3),
-              value: profile.lastname || "",
-              endAdornment: (
-                <InputAdornment position='end' className={classes.inputAdornment}>
-                  <Face className={classes.inputAdornmentIcon} />
-                </InputAdornment>
-              )
-            }}
-          />
-        </Grid>
-      </Grid>
-    )
-  }
-
-  renderBusiness() {
-    const { classes } = this.props;
-    const { profile } = this.state
-    console.log(profile)
-
-    return (
-      <Grid container justify='center' spacing={16}>
-        <Grid item xs={12} sm={12} md={12}>
-          <ProfileImageUpload />
-        </Grid>
-
-        <Grid item xs={12} sm={12} md={5}>
-          <CustomInput
-            success={this.state.businessnameState === 'success'}
-            error={this.state.businessnameState === 'error'}
-            labelText={<span>Business Name</span>}
-            id='businessname'
-            formControlProps={{ fullWidth: true }}
-            inputProps={{
-              onChange: event => this.change(event.target.value, 'businessname', 'length', 3),
-              value: profile.businessname || '',
-              endAdornment: (
-                <InputAdornment position='end' className={classes.inputAdornment}>
-                  <Face className={classes.inputAdornmentIcon} />
-                </InputAdornment>
-              )
-            }}
-          />
-        </Grid>
-
-        <Grid item xs={12} sm={12} md={5}>
-          <FormControl fullWidth className={classes.selectFormControl}>
-            <InputLabel htmlFor='simple-select' className={classes.selectLabel}>
-              Business Type
-            </InputLabel>
-
-            <Select
-              MenuProps={{ className: classes.selectMenu }}
-              classes={{ select: classes.select }}
-              value={businessTypes.indexOf(profile.businesstype)}
-              onChange={this.handleSimple}
-              inputProps={{
-                onChange: event => this.change(businessTypes[event.target.value], 'businesstype'),
-                name: "simpleSelect",
-                id: "simple-select"
-              }}
-            >
-              {businessTypes.map((v, i) => (
-                <MenuItem key={i} value={i} disabled={!i} classes={{ root: classes.selectMenuItem, selected: i ? classes.selectMenuItemSelected : undefined }}>{v}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Grid>
-
-        <Grid item xs={12} sm={12} md={3}>
-          <CustomInput
-            labelText={<span>Email</span>}
-            id='email'
-            formControlProps={{ fullWidth: true }}
-            inputProps={{
-              value: profile.email || '',
-              onChange: event => this.change(event.target.value, 'email', 'length', 3),
-              endAdornment: (
-                <InputAdornment position='end' className={classes.inputAdornment}>
-                  <Email className={classes.inputAdornmentIcon} />
-                </InputAdornment>
-              )
-            }}
-          />
-        </Grid>
-
-        <Grid item xs={12} sm={12} md={4}>
-          <CustomInput
-            labelText={<span>Website</span>}
-            id='website'
-            formControlProps={{ fullWidth: true }}
-            inputProps={{
-              onChange: event => this.change(event.target.value, 'website', 'length', 3),
-              value: profile.website || '',
-              endAdornment: (
-                <InputAdornment position='end' className={classes.inputAdornment}>
-                  <Web className={classes.inputAdornmentIcon} />
-                </InputAdornment>
-              )
-            }}
-          />
-        </Grid>
-
-        <Grid item xs={12} sm={12} md={3}>
-          <CustomInput
-            labelText={<span>Phone</span>}
-            id='phone'
-            formControlProps={{ fullWidth: true }}
-            inputProps={{
-              onChange: event => this.change(event.target.value, 'phone', 'length', 3),
-              value: profile.phone || '',
-              endAdornment: (
-                <InputAdornment position='end' className={classes.inputAdornment}>
-                  <Phone className={classes.inputAdornmentIcon} />
-                </InputAdornment>
-              )
-            }}
-          />
-        </Grid>
-
-        <Grid item xs={12} sm={12} md={10}>
-          <CustomInput
-            labelText='A brief description of your business.'
-            id='about-me'
-            formControlProps={{ fullWidth: true }}
-            inputProps={{
-              onChange: event => this.change(event.target.value, 'businessdescription'),
-              value: profile.businessdescription || '',
-            }}
-          />
-        </Grid>
-
-        <Grid item xs={12} sm={12} md={10}>
-          <BusinessLocationField
-            value={profile.businesslocation}
-            onChange={value => this.change(value, 'businesslocation')}
-            classes={classes}
-          />
-        </Grid>
-      </Grid>
-    )
-  }
 }
 
 const mapStateToProps = store => {
   return {
     wallet: store.data.wallet,
-    profile: store.ui.profile.profile || {}
+    profile: store.ui.profile.profile || {},
+    didId: store.ui.profile.didId,
+    loading: store.ui.profile.loading,
+    dirty: isDirty('profile')(store),
+    submitting: isSubmitting('profile')(store),
   };
 };
 
@@ -484,7 +239,8 @@ const mapDispatchToProps = {
   updateProfile,
   fetchProfile,
   fetchBalance,
-  push
+  push,
+  submit
 }
 
 export default compose(
