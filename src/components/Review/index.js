08c4bfb73d641df33ccfc3a3873b96e9a9270b9d
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-09-20 17:13:31 +0200

    show author profile picture and name in review widget

--- src/components/Review/index.js
+++ src/components/Review/index.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
-
-import StarRatingComponent from 'react-star-rating-component'
+// Material Components
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardHeader from '@material-ui/core/CardHeader'
 import Avatar from '@material-ui/core/Avatar'
-
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-
+// Components
+import StarRatingComponent from 'react-star-rating-component'
+import EditReview from 'components/EditReview'
+// Icons
 import DateRangeIcon from '@material-ui/icons/DateRange'
 import LinkIcon from '@material-ui/icons/Link'
 import CommentIcon from '@material-ui/icons/Comment'
@@ -18,13 +19,14 @@ import PlatformIcon from '@material-ui/icons/Store'
 import VerifiableIcon from '@material-ui/icons/VerifiedUser'
 import TokenIcon from '@material-ui/icons/PlusOne'
 import ViewMoreIcon from '@material-ui/icons/OpenInNew'
-
-import EditReview from 'components/EditReview'
-
+// Redux
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
-
+// Assets
+import defaultImage from 'images/default-avatar.png'
+// Helpers
 import { isArray, isNil, get } from 'lodash'
+import { Link } from 'react-router-dom'
 
 const starCount = 5
 class Review extends Component {
@@ -111,18 +113,20 @@ class Review extends Component {
   render() {
     const {
       review,
-      editing = false,
-      editingMultihash = null,
       detailed = false
     } = this.props
-    const maxStars = get(review, 'rating_details.max', starCount)
-    const max = maxStars > 0 ? maxStars : starCount
-    const editingThisReview = editing && editingMultihash === review.multihash
-    const authorName = review.author.name || review.author.did || (review.customer_signature && review.customer_signature.creator) || 'Anonymous'
+    let max = parseInt(get(review, 'rating_details.max', starCount), 10)
+    if (isNaN(max) || max < 1) max = starCount
+    const authorDid = review.author.did
+      || get(review, 'customer_signature.creator', 'Anonymous')
+    const authorName = get(review, 'author.profile.name')
+      || review.author.name
+      || authorDid
+    const avatarDataUrl = get(review, 'author.profile.avatarDataUrl', defaultImage)
     return (
       <Card>
         <CardHeader
-          avatar={<Avatar aria-label='review-author'>A</Avatar>}
+          avatar={<Avatar alt='author' src={avatarDataUrl}>A</Avatar>}
           title={
             <StarRatingComponent
               name='rating'
@@ -131,7 +135,7 @@ class Review extends Component {
               editing={false}
             />
           }
-          subheader={authorName}
+          subheader={<Link to={`/profile/${authorDid}`}>{authorName}</Link>}
         />
         {review.review && <CardContent>
           <List disablePadding>
@@ -186,13 +190,8 @@ class Review extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  editingMultihash: state.data.review.multihash,
-  editing: state.data.review.editing
-})
-
 const mapDispatchToProps = {
   push
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Review)
+export default connect(null, mapDispatchToProps)(Review)
