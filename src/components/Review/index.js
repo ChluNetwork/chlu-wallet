379753cfcd7a5d09118c850913fb5d0472ba71a1
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-09-21 11:52:12 +0200

    improve review UI

--- src/components/Review/index.js
+++ src/components/Review/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 // Material Components
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import CardHeader from '@material-ui/core/CardHeader'
+import { Card, CardContent, CardHeader, CardActionArea } from '@material-ui/core'
 import Avatar from '@material-ui/core/Avatar'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
@@ -15,6 +13,7 @@ import EditReview from 'components/EditReview'
 import DateRangeIcon from '@material-ui/icons/DateRange'
 import LinkIcon from '@material-ui/icons/Link'
 import CommentIcon from '@material-ui/icons/Comment'
+import RatingIcon from '@material-ui/icons/Star'
 import PlatformIcon from '@material-ui/icons/Store'
 import VerifiableIcon from '@material-ui/icons/VerifiedUser'
 import TokenIcon from '@material-ui/icons/PlusOne'
@@ -25,8 +24,8 @@ import { push } from 'react-router-redux'
 // Assets
 import defaultImage from 'images/default-avatar.png'
 // Helpers
-import { isArray, isNil, get } from 'lodash'
-import { Link } from 'react-router-dom'
+import { isArray, isNil, get, isEmpty } from 'lodash'
+import { Divider } from '@material-ui/core';
 
 const starCount = 5
 class Review extends Component {
@@ -110,56 +109,100 @@ class Review extends Component {
 
   openReview = () => this.props.push(`/review/${this.props.review.multihash}`)
 
+  openAuthorProfile = () => {
+    const { review } = this.props
+    const authorDid = review.author.did || get(review, 'customer_signature.creator')
+    if (authorDid) this.props.push(`/profile/${authorDid}`)
+  }
+
+  openSubjectProfile = () => {
+    const { review } = this.props
+    const subjectDid = review.subject.did || get(review, 'popr.vendor_did')
+    if (subjectDid) this.props.push(`/profile/${subjectDid}`)
+  }
+
   render() {
     const {
       review,
-      detailed = false
+      detailed = false,
+      showSubjectInfo = false,
+      showAuthorInfo = true
     } = this.props
-    let max = parseInt(get(review, 'rating_details.max', starCount), 10)
-    if (isNaN(max) || max < 1) max = starCount
+    // Author
     const authorDid = review.author.did
       || get(review, 'customer_signature.creator', 'Anonymous')
+    const isAuthorKnown = !isEmpty(authorDid)
     const authorName = get(review, 'author.profile.name')
       || review.author.name
       || authorDid
     const avatarDataUrl = get(review, 'author.profile.avatarDataUrl', defaultImage)
+    // Subject
+    const isSubjectKnown = !isEmpty(get(review, 'subject.profile'))
+    const subjectDid = review.subject.did
+      || get(review, 'popr.vendor_did', 'Unknown')
+    const subjectName = get(review, 'subject.profile.name')
+      || get(review, 'subject.profile.businessname')
+      || get(review, 'subject.profile.username')
+      || subjectDid
+    const subjectAvatarDataUrl = get(review, 'subject.profile.avatarDataUrl', defaultImage)
+    // Content (Text)
+    const contentTitle = get(review, 'review.title', '')
+    const contentText = get(review, 'review.text', '')
+    const contentPrimary = contentTitle || contentText
+    const contentSecondary = contentText && contentTitle ? contentText : ''
+    // Content (Rating)
+    let max = parseInt(get(review, 'rating_details.max', starCount), 10)
+    if (isNaN(max) || max < 1) max = starCount
+    const rating = get(review, 'rating_details.value', 0)
     return (
       <Card>
-        <CardHeader
-          avatar={<Avatar alt='author' src={avatarDataUrl}>A</Avatar>}
-          title={
-            <StarRatingComponent
-              name='rating'
-              starCount={max}
-              value={get(review, 'rating_details.value', 0)}
-              editing={false}
-            />
-          }
-          subheader={<Link to={`/profile/${authorDid}`}>{authorName}</Link>}
-        />
+        { isSubjectKnown && showSubjectInfo && <CardActionArea onClick={this.openSubjectProfile}>
+          <CardHeader
+            avatar={<Avatar alt='subject' src={subjectAvatarDataUrl}>V</Avatar>}
+            title={subjectName}
+            subheader='Received this Review'
+          />
+        </CardActionArea> }
+        { isSubjectKnown && showSubjectInfo && isAuthorKnown && showAuthorInfo && <Divider/> }
+        <CardActionArea onClick={this.openAuthorProfile}>
+          <CardHeader
+            avatar={<Avatar alt='author' src={avatarDataUrl}>A</Avatar>}
+            title={
+              <div>
+                <StarRatingComponent
+                  name='rating'
+                  starCount={max}
+                  value={rating}
+                  editing={false}
+                /> {contentPrimary}
+              </div>
+            }
+            subheader={`By ${authorName}`}
+            onClick={this.openAuthorProfile}
+          />
+        </CardActionArea>
         {review.review && <CardContent>
           <List disablePadding>
-            <ListItem>
+            { contentSecondary && <ListItem>
               <ListItemIcon><CommentIcon/></ListItemIcon>
               <ListItemText
-                primary={review.review.title || 'No title provided'}
-                secondary={review.review.text || 'No review provided'}
+                primary={contentSecondary}
               />
-            </ListItem>
-            <ListItem>
+            </ListItem> }
+            { detailed && <ListItem>
               <ListItemIcon><PlatformIcon/></ListItemIcon>
               <ListItemText
                 primary='Platform'
                 secondary={this.platform(review)}
               />
-            </ListItem>
-            <ListItem>
+            </ListItem> }
+            { detailed && <ListItem>
               <ListItemIcon><VerifiableIcon/></ListItemIcon>
               <ListItemText
                 primary='Verifiable'
                 secondary={isNil(review.verifiable) || !review.verifiable ? 'No' : 'Yes'}
               />
-            </ListItem>
+            </ListItem> }
             { detailed && <ListItem>
               <ListItemIcon><LinkIcon/></ListItemIcon>
               <ListItemText
